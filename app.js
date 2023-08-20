9b0199705b764dbdb60c705b77e21275edb520e1
Vyacheslav Morev <gromd96@gmail.com>
2023-08-20 19:57:04 +0300

    развернуть app.js

--- app.js
+++ app.js
@@ -1,13 +1,69 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standartHeaders: true,
+  legacyHeaders: false,
+});
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { login, createUser } = require('./controllers/users');
+const { validateUserBody, validateAuthentification } = require('./middlewares/validations');
+const { errorHandler } = require('./middlewares/error-handler');
+const auth = require('./middlewares/auth');
+const { NotFoundError } = require('./errors/not-found-error');
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
-
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  credentials: true,
+}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(express.json());
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
+
+app.use(requestLogger);
+app.use(limiter);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', validateUserBody, login);
+app.post('signup', validateAuthentification, createUser);
+
+app.use(auth);
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/movies'));
+
+app.get('/signout', (req, res) => {
+  res.clearCookie('jwt').send({ message: 'Выход' });
+});
+
+app.use(errorLogger);
+
+app.use((req, res, next) => next(new NotFoundError('неправильно указан путь')));
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
